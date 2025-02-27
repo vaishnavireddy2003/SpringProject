880bc944719a2a1dd8447cac9f2d1fdb7d1f998b
Vaishnavi Reddy <balgurivaishnavireddy@gmail.com>
2025-02-27 14:37:10 +0530

    commit

--- springboot/src/main/java/com/wipro/springboot/SpringbootApplication.java
+++ springboot/src/main/java/com/wipro/springboot/SpringbootApplication.java
@@ -7,7 +7,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @EnableDiscoveryClient
 @SpringBootApplication
 public class SpringbootApplication {
-
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootApplication.class, args);
 	}
